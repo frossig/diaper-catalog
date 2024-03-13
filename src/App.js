618c4dff8002e120de86dea83afa55224c0942c1
Fausto Rossi <frossi@tecnoazar.com.ar>
2024-03-12 23:16:31 -0300

    responsive, general styling, presnetation state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
 import DiapersCatalog from "./views/DiapersCatalog";
-import { ThemeProvider, createTheme } from '@mui/material';
-import { blue, green } from '@mui/material/colors';
+import { Box,  ThemeProvider, createTheme } from '@mui/material';
+import './App.css'
 
 
 const App = () => {
     const theme = createTheme({
        palette: {
     primary: {
-      main: blue[500],
+      main: "#ffefd5",
+      light: "whiteSmoke"
     },
-    tonalOffset: {
-      light: 0.1,
-      dark: 0.9,
+    secondary: {
+        main: "#71ab74"
     },
+    background: {
+        main: "rgb(209, 162, 162)"
+    }
   },
     });
 
+    const style = {
+        catalog: {
+            overflow: "auto",
+            height: "100vh",
+            width: "100vw",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: theme.palette.background.main,
+            '&::-webkit-scrollbar': {
+                width: '0.4em',
+                height: "0.4em"
+            },
+            '&::-webkit-scrollbar-track': {
+                background: theme.palette.grey[200]
+            },
+            '&::-webkit-scrollbar-thumb': {
+                borderRadius: "1rem",
+                background: theme.palette.grey[400]
+            },
+            '&::-webkit-scrollbar-thumb:hover': {
+                background: theme.palette.grey[400]
+            },
+            "&::-webkit-scrollbar-button": {
+                borderRadius: "1rem",
+                height: "50px"
+            }
+        }
+    }
+
     return (
         <ThemeProvider theme={theme}>
-            <DiapersCatalog />
+            <Box sx={style.catalog}>
+                <DiapersCatalog />
+            </Box>
         </ThemeProvider>
     );
 };
--- src/views/DiapersCatalog.jsx
+++ src/views/DiapersCatalog.jsx
@@ -1,11 +1,47 @@
-import { Box, Grid, Typography, useTheme } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import Loader from '../components/Loader';
 import DiaperCard from '../components/DiaperCard';
 
+
+const style = {
+    mainContainer: {
+        width: "95%",
+        height: "95%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    container: {
+        height: "95%",
+        width: "95%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column"
+    },
+    title: {
+        height: "5%",
+        width: "100%",
+        paddingLeft: "1.5rem"
+    },
+    gridContainer: {
+        height: "95%",
+        width: "95%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginLeft: "unset"
+        // flexDirection: "column"
+    },
+    gridItem: {
+        minWidth: "20%"
+    }
+
+}
+
 const DiapersCatalog = () => {
-    const theme = useTheme()
     const [isLoading, setIsLoading] = useState(false);
     const [diapers, setDiapers] = useState([]);
     const [groupedVariants, setGroupedVariants] = useState([]);
@@ -26,54 +62,30 @@ const DiapersCatalog = () => {
             .catch((err) => console.error("ERROR: ", err))
             .finally(() => setIsLoading(false))
 
+        //eslint-disable-next-line
     }, []);
-
-    // useEffect(() => {
-    //     const { variants } = diapers;
-    //     // console.log("🚀 ~ variants:", variants);
-
-    //     if (variants && variants.length > 0) {
-    //         const groupArr = [];
-
-    //         for (let i = 0; i < variants.length; i++) {
-    //             const variant = variants[i];
-    //             const { option1 } = variant;
-
-    //             if (!groupArr[option1]) {
-    //                 groupArr[option1] = [];
-    //             }
-    //             groupArr[option1].push(variant);
-    //         }
-    //         setGroupedVariants(groupArr)
-
-    //     }
-    // }, [diapers]);
 
     useEffect(() => {
         const { variants, image, vendor, presentationsBySizeInStock } = diapers;
-        console.log("🚀 ~ diapers:", diapers)
 
         if (variants && variants.length > 0) {
             const groupArr = [];
-
 
             for (let i = 0; i < variants.length; i++) {
                 const variant = variants[i];
                 const { option1 } = variant;
 
-                // Buscar si ya existe un grupo con la opción actual
                 const existingGroupIndex = groupArr.findIndex(group => group.option === option1);
+                // console.log("🚀 ~ existingGroupIndex:", existingGroupIndex)
 
                 if (existingGroupIndex === -1) {
-                    // Si no existe, crear un nuevo grupo
                     groupArr.push({ option: option1, variants: [variant], image, vendor, presentationsBySizeInStock });
                 } else {
-                    // Si existe, agregar la variante al grupo existente
                     groupArr[existingGroupIndex].variants.push(variant);
                 }
             }
 
-            console.log("🚀 ~ groupArr:", groupArr)
+            // console.log("🚀 ~ groupArr:", groupArr)
 
             setGroupedVariants(groupArr);
         }
@@ -84,14 +96,7 @@ const DiapersCatalog = () => {
 
 
     return (
-        <Box sx={{
-            width: "100vw",
-            height: "100vh",
-            backgroundColor: theme.palette.primary.dark,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-        }}
+        <Box sx={style.mainContainer}
         >
             {
                 isLoading ? (
@@ -99,13 +104,34 @@ const DiapersCatalog = () => {
                         loading={isLoading}
                     />
                 ) : (
-                    <Grid container spacing={2} justifyContent="center">
-                        {groupedVariants.map((group, idx) => (
-                            <Grid item key={idx}>
-                                <DiaperCard group={group} />
-                            </Grid>
-                        ))}
-                    </Grid>
+                    <Box
+                        sx={style.container}
+
+                    >
+                        <Grid
+                            sx={style.title}
+                        >
+                            {
+                                groupedVariants && (
+                                    <Typography variant='h5'>
+                                        Total Results: {groupedVariants.length}
+                                    </Typography>
+                                )
+                            }
+
+                        </Grid>
+                        <Grid
+                            container
+                            spacing={2}
+                            sx={style.gridContainer}
+                        >
+                            {groupedVariants.map((group, idx) => (
+                                <Grid item key={idx} sx={style.gridItem}>
+                                    <DiaperCard group={group} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
                 )
             }
         </Box>
