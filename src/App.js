46bed67f25dff49ce3b1a10a1fd07c593b494ee9
Fausto Rossi <frossi@tecnoazar.com.ar>
2024-03-12 23:24:23 -0300

    merge with develop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DiapersCatalog from "./views/DiapersCatalog";
-import { Box,  ThemeProvider, createTheme } from '@mui/material';
+import { Box, Container, ThemeProvider, createTheme } from '@mui/material';
+import { blue, green } from '@mui/material/colors';
 import './App.css'
 
 
@@ -8,51 +9,21 @@ const App = () => {
     const theme = createTheme({
        palette: {
     primary: {
-      main: "#ffefd5",
-      light: "whiteSmoke"
-    },
-    secondary: {
-        main: "#71ab74"
+      main: blue[500],
     },
     background: {
-        main: "rgb(209, 162, 162)"
-    }
+        main: rgb(209, 162, 162)
+    },
+    tonalOffset: {
+      light: 0.1,
+      dark: 0.9,
+    },
   },
     });
 
-    const style = {
-        catalog: {
-            overflow: "auto",
-            height: "100vh",
-            width: "100vw",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: theme.palette.background.main,
-            '&::-webkit-scrollbar': {
-                width: '0.4em',
-                height: "0.4em"
-            },
-            '&::-webkit-scrollbar-track': {
-                background: theme.palette.grey[200]
-            },
-            '&::-webkit-scrollbar-thumb': {
-                borderRadius: "1rem",
-                background: theme.palette.grey[400]
-            },
-            '&::-webkit-scrollbar-thumb:hover': {
-                background: theme.palette.grey[400]
-            },
-            "&::-webkit-scrollbar-button": {
-                borderRadius: "1rem",
-                height: "50px"
-            }
-        }
-    }
-
     return (
         <ThemeProvider theme={theme}>
-            <Box sx={style.catalog}>
+            <Box sx={{height:"100vh", width:"100vw",  display:"flex",alignItems:"center",justifyContent:"center", backgroundColor:theme.palette}}>
                 <DiapersCatalog />
             </Box>
         </ThemeProvider>
